2a36995ce721cc86ecf83eef08125f8e18f8a83b
Ivan Lopes <lopesivan.ufrj@gmail.com>
2026-06-30 07:45:48 -0300

    :warning:  Arquivo system/opt/sdk/bin/sdk.sh marcado para deleção

--- system/opt/sdk/bin/sdk.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-NAME=$(grep NAME /etc/sdk/config | sed 's/.*=\s*\([^\t ]*\)\s*/\1/')
-VERSION=$(grep VERSION /etc/sdk/config | sed 's/.*=\s*\([^\t ]*\)\s*/\1/')
-
-echo ===============================================
-echo $NAME-$VERSION
-echo ===============================================
-
-exit 0
