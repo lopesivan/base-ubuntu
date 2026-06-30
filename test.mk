425c8f95a0c8dc7e24013a4516a360ff05eb7e43
Ivan Lopes <lopesivan.ufrj@gmail.com>
2026-06-30 09:37:20 -0300

    :style: Atualiza o estilo do arquivo test.mk

--- test.mk
+++ test.mk
@@ -3,3 +3,33 @@
 
 test:
 	curl http://localhost:8989/
+
+
+clone:
+	git clone git://localhost:9418/teste.git
+
+repo:
+	mkdir -p repos
+	git init --bare repos/teste.git
+	touch repos/teste.git/git-daemon-export-ok
+	git --git-dir=repos/teste.git config daemon.receivepack true
+
+git-info:
+	$(DOCKER) exec -it $(CONTAINER_NAME) ls -l /srv/git
+	$(DOCKER) exec -it $(CONTAINER_NAME) ls -l /srv/git/teste.git
+	$(DOCKER) exec -it $(CONTAINER_NAME) ls -l /srv/git/teste.git/git-daemon-export-ok
+
+git-list:
+	$(DOCKER) exec -it $(CONTAINER_NAME) bash -lc '
+	echo "--- /srv/git ---"
+	ls -la /srv/git
+
+	echo "--- teste.git ---"
+	ls -la /srv/git/teste.git || true
+
+	echo "--- config ---"
+	cat /srv/git/teste.git/config || true
+
+	echo "--- daemon marker ---"
+	ls -la /srv/git/teste.git/git-daemon-export-ok || true
+	'
