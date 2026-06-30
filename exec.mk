8e5c77fd9d9eddeacb74d7cae53b27b4011d4e51
Ivan Lopes <lopesivan.ufrj@gmail.com>
2026-06-30 10:19:19 -0300

    :style: Atualiza o estilo do arquivo exec.mk

--- exec.mk
+++ exec.mk
@@ -1,6 +1,6 @@
 exec-python:
-	$(DOCKER_COMPOSE) exec $(CONTAINER_NAME) as-user python3
+	$(DOCKER) exec -it $(CONTAINER_NAME) as-user python3
 exec:
-	$(DOCKER_COMPOSE) exec $(CONTAINER_NAME) as-user bash -l
+	$(DOCKER) exec -it $(CONTAINER_NAME) as-user bash -l
 exec-root:
 	$(DOCKER) exec -it -u root $(CONTAINER_NAME) bash
